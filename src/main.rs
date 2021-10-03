ca51d1c5bca57e735242f74d0667ffd3d8354b6e
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-10-03 12:09:37 -0300

    feat: contains a string
    Contains text in file

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
-        println!("{}", args[1]);
+    if args.len() == 3 {
         let path = Path::new(&args[1]);
         let display = path.display();
 
@@ -18,7 +17,12 @@ fn main() {
         let mut s = String::new();
         match file.read_to_string(&mut s) {
             Err(why) => panic!("couldn't read {}: {}", display, why),
-            Ok(_) => print!("{} contains:\n{}", display, s),
+            Ok(_) => {
+                print!("{} contains:\n{}", display, s);
+                if s.contains(&args[2]) {
+                    print!("{} have: {}", display, args[2]);
+                }
+            },
         }
     }
 }
