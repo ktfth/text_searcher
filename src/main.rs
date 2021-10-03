55835559d4761ca4e494c2348eb8cae03591858d
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-10-03 14:30:13 -0300

    feat: traverse directory
    Find in all files present on the directory

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,57 @@
 use std::env;
-use std::fs::File;
 use std::{fs, io};
-use std::io::prelude::*;
+use std::fs::File;
 use std::path::Path;
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+fn collect_file_from_dir(dir: &Path) -> io::Result<Vec<PathBuf>> {
+    let mut result: Vec<PathBuf> = vec![];
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                result.extend(collect_file_from_dir(&path)?);
+            } else {
+                result.push(path);
+            }
+        }
+    }
+    Ok(result)
+}
+
+fn search_content(entry: String) {
+    let args: Vec<String> = env::args().collect();
+    let path = Path::new(&entry);
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(_) => print!(""),
+        Ok(_) => {
+            if s.contains(&args[1]) {
+                print!("{} contains:\n{}", display, s);
+                print!("{} have: {}", display, args[1]);
+            }
+        }
+    }
+}
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() == 2 {
-        let mut entries = fs::read_dir(".")?
-            .map(|res| res.map(|e| e.path()))
-            .collect::<Result<Vec<_>, io::Error>>()?;
-        entries.sort();
-        for entry in entries {
-            if !entry.is_dir() {
-                let path = Path::new(&entry);
-                let display = path.display();
-
-                let mut file = match File::open(&path) {
-                    Err(why) => panic!("couldn't open {}: {}", display, why),
-                    Ok(file) => file,
-                };
-
-                let mut s = String::new();
-                match file.read_to_string(&mut s) {
-                    Err(why) => panic!("couldn't read {}: {}", display, why),
-                    Ok(_) => {
-                        if s.contains(&args[1]) {
-                            print!("{} contains:\n{}", display, s);
-                            print!("{} have: {}", display, args[1]);
-                        }
-                    },
-                }
+        let current_dir = Path::new(".");
+        let paths = collect_file_from_dir(current_dir);
+        for path in paths {
+            for entry in path {
+                let entry_path = entry.to_string_lossy().to_string();
+                search_content(entry_path);
             }
         }
     }
